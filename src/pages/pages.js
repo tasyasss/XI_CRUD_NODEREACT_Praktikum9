3f08f88f9ac91822c755d16984678686be45e6b4
unknown <tasyasakina0703@gmail.com>
2021-01-21 21:55:07 +0700

    crud node react

---
+++ src/pages/pages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Pegawai from './pegawai'
+const Pages = () => (
+ <Switch>
+ <Route path='/pegawai' component={Pegawai} />
+ </Switch>
+)
+export default Pages
